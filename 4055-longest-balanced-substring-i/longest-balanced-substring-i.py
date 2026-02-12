3880ed78b5b90504506f3e8cde7bf0e2a3fe6fbb
Ankush Rai <iamankushrai@gmail.com>
2026-02-12 17:46:39 +0530

    Time: 4410 ms (20.63%) | Memory: 19.4 MB (44.33%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+
+        def is_same_freq(char_count):
+            for el,count in char_count.items():
+                first_element_count= next(iter(char_count.values()))
+                if count!=first_element_count:
+                    return False
+            return True
+        result_count=0
+        n=len(s)
+        for i in range(n):
+            char_count=defaultdict(int)
+            for j in range(i,n):
+                char_count[s[j]]+=1
+                if is_same_freq(char_count):
+                    result_count=max(result_count,j-i+1)
+        
+        return result_count
