d65a9ebba17c886d948a174f71e674e6d45eacc5
Ankush Rai <iamankushrai@gmail.com>
2026-02-17 10:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (13.49%) - LeetSync

---
+++ 401-binary-watch/binary-watch.py
@@ -0,0 +1,9 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        result=[]
+        for hour in range(12):
+            for minute in range(60):
+                if bin(hour).count('1') + bin(minute).count('1')==turnedOn:
+                    result.append(f"{hour}:{minute:02d}")
+        
+        return result
